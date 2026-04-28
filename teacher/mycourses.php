93444549be7d5a41a10c38b9d6ee47d094be4e1e
jihanejador <jihanejador@gmail.com>
2026-04-28 12:17:16 +0100

    verfier la securite

--- teacher/mycourses.php
+++ teacher/mycourses.php
@@ -1,1 +1,11 @@
 <?php
+session_start();
+
+require_once '../config/db.php';
+
+if(!isset($_SESSION['user_id'])|| $_SESSION['role'] !=='prof'){
+    header("Location: ../auth/login.php");
+    exit;
+}
+$prof_id = $_SESSION['user_id'];
+
