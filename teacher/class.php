b94cc4cde4a779615f7c18614eee74db97711666
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-29 10:33:34 +0100

    modifier les nom de colums pour la nouvelle base de données

--- teacher/class.php
+++ teacher/class.php
@@ -3,20 +3,26 @@
 session_start();
 
 require_once '../db.php';
-if (!isset($_SESSION['user_id'])){
-header("location:../login.php");
-exit();
-}
-$professor=$_SESSION['user_id'];
 
-$sql= "SELECT DISTINCT  classes.id, classes.nom, classes.classeroom_number
-from classes
-join courses
-on classes.id =courses.classe_id
+if (!isset($_SESSION['user_id'])) {
+    header("location:../login.php");
+    exit();
+}
+
+$professor = $_SESSION['user_id'];
+
+$sql = "
+SELECT DISTINCT classes.id, classes.name, classes.classroom_number
+FROM classes
+JOIN courses ON classes.id = courses.classe_id
 WHERE courses.prof_id = :prof_id
 ";
 
-$stm=$pdo->prepare($sql);
-$stm->execute([ 'prof_id'=>$professor ]);
+$stm = $pdo->prepare($sql);
 
-$classes=$stm->fetchAll(PDO::FETCH_ASSOC);
+$stm->execute([
+    'prof_id' => $professor
+]);
+
+$classes = $stm->fetchAll(PDO::FETCH_ASSOC);
+?>
