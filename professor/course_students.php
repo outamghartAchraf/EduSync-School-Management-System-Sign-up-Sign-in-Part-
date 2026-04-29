1cf5034b8a8b4fe348288906cc806ed8235876d9
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-29 17:36:41 +0100

    passer le requette pour récupérer les données

--- professor/course_students.php
+++ professor/course_students.php
@@ -6,3 +6,17 @@ if (!isset($_SESSION['user']['id'])) {
     header("Location: ../login.php");
     exit();
 }
+
+$course_id = $_GET['course_id'];
+
+$sql = "
+SELECT 
+    enrollments.id,
+    users.firstname,
+    users.lastname,
+    enrollments.status
+FROM enrollments
+JOIN students ON students.id = enrollments.student_id
+JOIN users ON users.id = students.user_id
+WHERE enrollments.course_id = :course_id
+";
