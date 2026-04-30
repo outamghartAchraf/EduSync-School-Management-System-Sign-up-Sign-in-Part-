8ed43b7eb1a056b507072c1935e24ca87c91727f
jihanejador <jihanejador@gmail.com>
2026-04-30 11:19:50 +0100

    modification sur les conditions

--- professor/mycourses.php
+++ professor/mycourses.php
@@ -2,12 +2,9 @@
 session_start();
 require_once '../config/db.php';
 
-if(!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'prof'){
-    header("Location: ../auth/login.php");
-    exit;
-}
 
-$prof_id = $_SESSION['user_id'];
+
+$prof_id = $_SESSION['user']['id'];
 
 try {
 
