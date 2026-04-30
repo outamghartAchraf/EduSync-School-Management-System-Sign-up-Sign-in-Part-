446f3745e1fb62a79332e64fd16d07f1814d1945
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-30 10:37:05 +0100

    Vérifier l'existance de ID et Statu

--- professor/modifier_status.php
+++ professor/modifier_status.php
@@ -7,3 +7,15 @@ if (!isset($_SESSION['user']['id'])) {
     header("Location: ../login.php");
     exit();
 }
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+} else {
+    $id = null;
+}
+
+if (isset($_GET['status'])) {
+    $status = $_GET['status'];
+} else {
+    $status = null;
+}
